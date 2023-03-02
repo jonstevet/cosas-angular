a948fadebaccdf7006443562241610eb305d8e46
JonStevet <JonSteve@outlook.com>
2023-03-02 01:02:06 -0500

    modifier 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
 import { Component } from '@angular/core';
+import {product} from './product.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
+
 export class AppComponent {
-  title = 'my-store';
+  name:string = 'JonSteve';
+  age:number = 34;
+  imgurl:string = 'https://www.w3schools.com/w3images/avatar2.png';
+  btn101State:boolean = true;
+  names:string[] = ['Jon', 'Steve', 'John', 'Stevie', 'Johnny'];
+  newName:string = '';
+
+  person = {
+    name: 'JonSteve',
+    age: 34,
+    avatar: 'https://www.w3schools.com/w3images/avatar2.png'
+  };
+
+  products:product[] = [
+    {name: 'Product 1', price: 100, img: './assets/images/avatar.png'},
+    {name: 'Product 2', price: 200, img: './assets/images/avatar.png'},
+    {name: 'Product 3', price: 300, img: './assets/images/avatar.png'},
+    {name: 'Product 4', price: 400, img: './assets/images/avatar.png'},
+    {name: 'Product 5', price: 500, img: './assets/images/avatar.png'},
+  ];
+
+  toggleBtn(): void {
+    this.btn101State = !this.btn101State;
+  }
+
+  ageIncrease(): void {
+    this.person.age++;
+  }
+
+  onScroll(event: Event): void {
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+
+  onPress(event: KeyboardEvent): void {
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+    console.log(event.key);
+  }
+
+  onPressAddName(): void {
+      this.names.push(this.newName);
+      this.newName = '';
+  }
+
+  onPressRemoveName(index: number): void {
+      this.names.splice(index, 1);
+  }
 }
